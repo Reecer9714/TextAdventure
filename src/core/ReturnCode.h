80418aed571bd1fedb4334a5f219d19a85b95c19
Reece Burch <reecer97@yahoo.com>
2021-05-15 14:26:20 -0500

    Formatted files

--- src/core/ReturnCode.h
+++ src/core/ReturnCode.h
@@ -1,11 +1,15 @@
 #ifndef RETURN_CODE_H
 #define RETURN_CODE_H
 
-#include <stdint.h>
+#include <cstddef>
 #include <iostream>
 #include <map>
+#include <stdint.h>
 
 static std::map<uint32_t, std::string> gErrorStrings;
+
+constexpr size_t SYSTEM_OFFSET    = 16;
+constexpr size_t SUBSYSTEM_OFFSET = 8;
 
 enum class System : uint8_t
 {
@@ -18,47 +22,45 @@ enum class System : uint8_t
 struct SubSystem
 {
     constexpr operator uint8_t() const { return code; }
-    constexpr explicit SubSystem(uint8_t i) : code(i) {}
+    constexpr explicit SubSystem( uint8_t i )
+        : code( i )
+    {
+    }
+
 private:
     uint8_t code;
 };
 
 struct ReturnCode
 {
-    System system;
+    System    system;
     SubSystem subsystem{ 0 };
-    uint8_t errorcode;
-    uint32_t hash;
+    uint8_t   errorcode;
+    uint32_t  hash;
 
-    constexpr ReturnCode(const System sys, const uint8_t code) : system(sys), errorcode(code), hash(Hash())
-    {
-    };
-    
-    constexpr ReturnCode(const System sys, const SubSystem sub, const uint8_t code) : system(sys), subsystem(sub), errorcode(code), hash(Hash())
-    {
-    };
+    constexpr ReturnCode( const System sys, const uint8_t code )
+        : system( sys )
+        , errorcode( code )
+        , hash( Hash() ){};
 
-    inline bool Success() const
-    {
-        return ( int( errorcode ) == 0 );
-    };
+    constexpr ReturnCode( const System sys, const SubSystem sub, const uint8_t code )
+        : system( sys )
+        , subsystem( sub )
+        , errorcode( code )
+        , hash( Hash() ){};
 
-    inline bool Failed() const
-    {
-        return ( int( errorcode ) != 0 );
-    };
+    inline bool Success() const { return ( int( errorcode ) == 0 ); };
 
-    inline constexpr uint32_t Hash() 
+    inline bool Failed() const { return ( int( errorcode ) != 0 ); };
+
+    inline constexpr uint32_t Hash() const
     {
-        return  ( static_cast<uint8_t>( system )    << 16 ) +
-                ( static_cast<uint8_t>( subsystem ) << 8 ) +
-                ( static_cast<uint8_t>( errorcode ) );
+        return ( static_cast<uint8_t>( system ) << SYSTEM_OFFSET ) +
+               ( static_cast<uint8_t>( subsystem ) << SUBSYSTEM_OFFSET ) +
+               ( static_cast<uint8_t>( errorcode ) );
     };
 
-    inline std::string ToString() const
-    {
-        return gErrorStrings.find( hash )->second;
-    };
+    inline std::string ToString() const { return gErrorStrings.find( hash )->second; };
 
     inline bool operator==( const ReturnCode& rhs ) const
     {
@@ -66,9 +68,8 @@ struct ReturnCode
     }
 };
 
-static constexpr const ReturnCode SUCCESS = ReturnCode(System::COMMON, 0x0000);
-static constexpr const ReturnCode NOT_IMPLEMENTED = ReturnCode(System::COMMON, 0x0001);
-static constexpr const ReturnCode NULLPTR_INPUT = ReturnCode(System::COMMON, 0x0002);
-
+static constexpr const ReturnCode SUCCESS         = ReturnCode( System::COMMON, 0x0000 );
+static constexpr const ReturnCode NOT_IMPLEMENTED = ReturnCode( System::COMMON, 0x0001 );
+static constexpr const ReturnCode NULLPTR_INPUT   = ReturnCode( System::COMMON, 0x0002 );
 
 #endif
--- src/data/DataError.h
+++ src/data/DataError.h
@@ -5,18 +5,15 @@
 
 struct DataSubSystem : public SubSystem
 {
-    constexpr DataSubSystem(uint8_t n): SubSystem(n) {};
-    static const DataSubSystem
-        LOCATION,
-        ENTITY,
-        ITEM;
+    constexpr DataSubSystem( uint8_t n )
+        : SubSystem( n ){};
+    static const DataSubSystem LOCATION, ENTITY, ITEM;
 };
 
-inline constexpr const DataSubSystem
-    DataSubSystem::LOCATION{ 0 },
-    DataSubSystem::ENTITY{ 1 },
+inline constexpr const DataSubSystem DataSubSystem::LOCATION{ 0 }, DataSubSystem::ENTITY{ 1 },
     DataSubSystem::ITEM{ 2 };
 
-static constexpr const ReturnCode DUPLICATE_CONNECTION = ReturnCode(System::DATA, DataSubSystem::LOCATION, 0x0001);
+static constexpr const ReturnCode DUPLICATE_CONNECTION =
+    ReturnCode( System::DATA, DataSubSystem::LOCATION, 0x0001 );
 
 #endif // DATAERROR_H
